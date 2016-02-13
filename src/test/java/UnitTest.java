db129a377a4408970f2eb4438f819ebaafe181a7
Brad Pizzimenti <brad@gennakersystems.com>
2016-02-13 15:34:23 -0800

    delete boilerplate testspec commentblock and fix indentation of unit test

--- src/test/java/UnitTest.java
+++ src/test/java/UnitTest.java
@@ -3,18 +3,10 @@ import static org.junit.Assert.*;
 
 public class UnitTest {
 
-@Test
-public void VowelsToDashes_test_t_st() {
-  App testApp = new App();
-  assertEquals("sn-ggl-", testApp.VowelsToDashes("snuggle"));
-}
-
-/*
-@Test
-public void giveChange_25cents_1quarter() {
-  App testApp = new App();
-  assertEquals("{Nickels=0, Dimes=0, Quarters=1, Pennies=0}", testApp.giveChange(25).toString());
-}
-*/
+  @Test
+  public void VowelsToDashes_test_t_st() {
+    App testApp = new App();
+    assertEquals("sn-ggl-", testApp.VowelsToDashes("snuggle"));
+  }
 
 }
