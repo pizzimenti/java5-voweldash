262be59c67fd51e8a784803ea8972f4134f0bd28
Brad Pizzimenti <brad@gennakersystems.com>
2016-02-14 15:21:58 -0800

    WIP three different failing methods for Hint function

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -40,5 +40,34 @@ public class App {
     return textInput;
   } //end of VowelsToDashes
 
+  public static String Hint (String result, String textInput) {
 
+  //this method causes: not a statement and illegal start of expression
+    String textInput = "snuggle"; //defined just for repl testing
+    String result = "sn-ggl-"; //defined just for repl testing
+    for(int i = 0; i < textInput.length(); i++) {
+      if (result.charAt(i) == '-') {
+        result.charAt(i) == textInput.charAt(i);
+      }
+    }
+
+  //this method causes: java.lang.ArrayIndexOutOfBoundsException: 115
+    String textInput = "snuggle"; //defined just for repl testing
+    String result = "sn-ggl-"; //defined just for repl testing
+    char[] resultCharred = result.toCharArray();
+    for(char letter: resultCharred) {
+      result = result.replace('-',textInput.charAt(letter));
+    }
+
+
+  //this method runs without errors but only places the first character of textInput into the dashed result where there are dashes, not the corresponding character
+  String textInput = "snuggle"; //defined just for repl testing
+  String result = "sn-ggl-"; //defined just for repl testing
+  char[] resultCharred = result.toCharArray();
+  for(char letter: resultCharred) {
+    result = result.replace('-',letter);
+  }
+
+
+  } //end of Hint
 } //end of App class
